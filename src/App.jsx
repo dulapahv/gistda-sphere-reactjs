39d4bc14ac585cefb8f5809b9a80ff7853ba0506
Dulapah Vibulsanti <dulapah6@gmail.com>
2024-09-25 13:35:56 +0100

    feat: add marker and search example

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useRef } from 'react';
+import { useRef, useState, useCallback } from 'react';
 
 import { SphereMap } from './map';
 
@@ -6,13 +6,16 @@ export default function App() {
   const mapRef = useRef();
   const sphereRef = useRef();
 
+  const [search, setSearch] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+
   const mapKey = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
 
-  function onMapLoad() {
+  const onMapLoad = useCallback(() => {
     if (!mapRef.current) return;
 
     mapRef.current.Layers.setBase(sphereRef.current.Layers.STREETS);
-  }
+  }, []);
 
   function setLocationZoom() {
     if (!mapRef.current) return;
@@ -50,11 +53,72 @@ export default function App() {
     ).innerHTML = `Status: Theme changed to ${mapRef.current.enableFilter()}`;
   }
 
+  function addMarker() {
+    // https://sphere.gistda.or.th/docs/js/marker
+    // https://api.sphere.gistda.or.th/map/doc.html#Marker
+    const marker = new sphereRef.current.Marker({ lon: 100.56, lat: 13.74 });
+    mapRef.current.Overlays.add(marker);
+
+    document.getElementById('status').innerHTML =
+      'Status: Marker added at lon: 100.56, lat: 13.74';
+  }
+
+  function searchKeyword() {
+    // https://sphere.gistda.or.th/docs/web-service/search
+    const searchUrl = `https://api.sphere.gistda.or.th/services/search/search?keyword=${search}&key=${mapKey}`;
+    fetch(searchUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        setSearchResults(data.data);
+      });
+  }
+
+  function goToSearchResult(result) {
+    // go to location
+    mapRef.current.goTo({
+      center: { lon: result.lon, lat: result.lat },
+      zoom: 14,
+    });
+
+    // add marker
+    const marker = new sphereRef.current.Marker({
+      lon: result.lon,
+      lat: result.lat,
+    });
+    mapRef.current.Overlays.add(marker);
+
+    document.getElementById(
+      'status'
+    ).innerHTML = `Status: Go to ${result.name} and add marker at lon: ${result.lon}, lat: ${result.lat}`;
+  }
+
   return (
     <div style={{ height: '80vh' }}>
-      <button onClick={setLocationZoom}>Set Location and Zoom</button>
-      <button onClick={changeLanguage}>Change to English</button>
-      <button onClick={changeTheme}>Change to Dark</button>
+      <div>
+        <button onClick={setLocationZoom}>Set Location and Zoom</button>
+        <button onClick={changeLanguage}>Change to English</button>
+        <button onClick={changeTheme}>Change to Dark</button>
+        <button onClick={addMarker}>Add Marker</button>
+      </div>
+      <div>
+        <input
+          type='text'
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          placeholder='Search... (e.g. กรุงเทพ)'
+        />
+        <button onClick={searchKeyword}>Search</button>
+      </div>
+      <p>Search Result:</p>
+      <ul>
+        {searchResults.map((result) => (
+          <li key={result.id}>
+            <button onClick={() => goToSearchResult(result)}>
+              {result.name}
+            </button>
+          </li>
+        ))}
+      </ul>
       <p id='status'>Status: </p>
 
       <div style={{ height: '100%', width: '100%' }}>
